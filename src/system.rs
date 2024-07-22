3e7cb449f2160789b346aa41ee4d0f1b971ae556
dom <domwilson95@hotmail.co.uk>
2024-07-22 23:25:06 +0100

    feat: linear system

--- src/system.rs
+++ src/system.rs
@@ -1,4 +1,4 @@
-use nalgebra::{SMatrix, SVector};
+use nalgebra::{RealField, SMatrix, SVector};
 
 pub trait System<T, const N: usize> {
     /// transition to the next state, returning a reference to it
@@ -17,4 +17,27 @@ struct LinearSystem<T, const N: usize> {
     x: SVector<T, N>,
     F: SMatrix<T, N, N>,
     Q: SMatrix<T, N, N>,
+}
+
+impl<T: RealField + Copy, const N: usize> System<T, N> for LinearSystem<T, N> {
+    fn step(&mut self) -> &SVector<T, N> {
+        self.x = self.F * self.x;
+        &self.x
+    }
+
+    fn transition(&self) -> &SMatrix<T, N, N> {
+        &self.F
+    }
+
+    fn covariance(&self) -> &SMatrix<T, N, N> {
+        &self.Q
+    }
+
+    fn state(&self) -> &SVector<T, N> {
+        &self.x
+    }
+
+    fn state_mut(&mut self) -> &mut SVector<T, N> {
+        &mut self.x
+    }
 }
