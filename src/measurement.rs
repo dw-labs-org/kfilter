138c9a35d5ecbbfd1ef0ef33e394bdd17a7a99b8
dom <domwilson95@hotmail.co.uk>
2024-07-26 09:10:09 +0100

    fix: nonlinear measurement constructor + measurement docs

--- src/measurement.rs
+++ src/measurement.rs
@@ -1,5 +1,15 @@
+//! A measurement is the input to the Kalman filter during the update stage.
+//! It is defined by the measurement vector z (sensor readings), the observation
+//! matrix H (or function h(x)) and the measurment noise R.
+//!
+//! For use with the Kalman filter, [Measurement] must be implemented. The
+//! vast majority of filters can use [LinearMeasurement] or even [NonLinearMeasurement]
+//! if required.
+
 use nalgebra::{RealField, SMatrix, SVector};
 
+/// Trait that defines the functionality for the [Kalman](crate::kalman::Kalman) filter
+/// to interact with a measurement.
 pub trait Measurement<T, const N: usize, const M: usize> {
     /// Calculate the innovation (y) based on the measurement and the observation mapping
     fn innovation(&self, x: &SVector<T, N>) -> SVector<T, M>;
@@ -11,18 +21,26 @@ pub trait Measurement<T, const N: usize, const M: usize> {
     fn observation_transpose(&self) -> &SMatrix<T, N, M>;
     /// Set the measurment (z) value
     fn set_measurement(&mut self, z: SVector<T, M>);
+    /// Set the observation matrix. Ensures transpose is updated.
+    #[allow(non_snake_case)]
+    fn set_observation(&mut self, H: SMatrix<T, M, N>);
 }
 
 #[allow(non_snake_case)]
+/// A linear measurement defined by the observation matrix H and noise matrix R.
+/// Implements the innovation function y = z - H * x.
 pub struct LinearMeasurement<T, const N: usize, const M: usize> {
+    /// Observation / measurement. Can be modifed directly to set new value.
     pub z: SVector<T, M>,
     H: SMatrix<T, M, N>,
     H_t: SMatrix<T, N, M>,
+    /// Measurement noise. Can be modifed directly to set new value.
     pub R: SMatrix<T, M, M>,
 }
 
 impl<T: RealField, const N: usize, const M: usize> LinearMeasurement<T, N, M> {
     #[allow(non_snake_case)]
+    /// Create new measurement with observation matrix H, noise matrix R and measurement z.
     pub fn new(H: SMatrix<T, M, N>, R: SMatrix<T, M, M>, z: SVector<T, M>) -> Self {
         Self {
             z,
@@ -30,12 +48,6 @@ impl<T: RealField, const N: usize, const M: usize> LinearMeasurement<T, N, M> {
             H,
             R,
         }
-    }
-
-    #[allow(non_snake_case)]
-    pub fn set_observation(&mut self, H: SMatrix<T, M, N>) {
-        self.H_t = H.transpose();
-        self.H = H;
     }
 }
 
@@ -61,30 +73,39 @@ impl<T: RealField + Copy, const N: usize, const M: usize> Measurement<T, N, M>
     fn set_measurement(&mut self, z: SVector<T, M>) {
         self.z = z;
     }
+    #[allow(non_snake_case)]
+    fn set_observation(&mut self, H: SMatrix<T, M, N>) {
+        self.H_t = H.transpose();
+        self.H = H;
+    }
 }
 
 /// Function that returns the predicted value of the measurment based on the current state
 pub type Prediction<T, const N: usize, const M: usize> = fn(&SVector<T, N>) -> SVector<T, M>;
 
 /// A non linear measurement that uses a prediction function to calculate h(x)
-/// H and R must be updated before being passed to kalman filter
+/// H and R must be updated before being passed to [Kalman](crate::kalman::Kalman) filter
 #[allow(non_snake_case)]
 pub struct NonLinearMeasurement<T, const N: usize, const M: usize> {
+    /// Observation / measurement. Can be modifed directly to set new value.
     pub z: SVector<T, M>,
     H: SMatrix<T, M, N>,
     H_t: SMatrix<T, N, M>,
+    /// Measurement noise. Can be modifed directly to set new value.
     pub R: SMatrix<T, M, M>,
     /// Calculates the predicted value of z. i.e h(x)
     prediction_fn: Prediction<T, N, M>,
 }
 
 impl<T: RealField, const N: usize, const M: usize> NonLinearMeasurement<T, N, M> {
-    pub fn new(prediction_fn: Prediction<T, N, M>) -> Self {
+    /// Create new [NonLinearMeasurement].
+    #[allow(non_snake_case)]
+    pub fn new(prediction_fn: Prediction<T, N, M>, R: SMatrix<T, M, M>, z: SVector<T, M>) -> Self {
         Self {
-            z: SMatrix::zeros(),
+            z,
             H: SMatrix::zeros(),
             H_t: SMatrix::zeros(),
-            R: SMatrix::zeros(),
+            R,
             prediction_fn,
         }
     }
@@ -111,5 +132,10 @@ impl<T: RealField + Copy, const N: usize, const M: usize> Measurement<T, N, M>
 
     fn set_measurement(&mut self, z: SVector<T, M>) {
         self.z = z;
+    }
+    #[allow(non_snake_case)]
+    fn set_observation(&mut self, H: SMatrix<T, M, N>) {
+        self.H_t = H.transpose();
+        self.H = H;
     }
 }
