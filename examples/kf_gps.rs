f025e12393bd5aebad817051852da75c8eda0f78
dom <dom@dwlabs.co.uk>
2025-08-27 15:18:10 +0100

    fix remove plotters from example for now

--- examples/kf_gps.rs
+++ examples/kf_gps.rs
@@ -6,12 +6,12 @@ use kfilter::{
     KalmanLinearNoInput, KalmanPredict, KalmanUpdate,
 };
 use nalgebra::{Matrix1, Matrix1x2, SMatrix, Vector2};
-use plotters::{
-    chart::ChartBuilder,
-    prelude::{BitMapBackend, IntoDrawingArea},
-    series::LineSeries,
-    style::{full_palette::BLUE, BLACK, GREEN, RED, WHITE},
-};
+// use plotters::{
+//     chart::ChartBuilder,
+//     prelude::{BitMapBackend, IntoDrawingArea},
+//     series::LineSeries,
+//     style::{full_palette::BLUE, BLACK, GREEN, RED, WHITE},
+// };
 use rand_distr::Distribution;
 #[allow(non_snake_case)]
 fn main() {
@@ -91,67 +91,67 @@ fn main() {
         );
     }
 
-    let t = (0..n_samples).map(|x| x as f64 * dt).collect::<Vec<f64>>();
-    let x_actual: Vec<f64> = state.iter().map(|s| s.x).collect();
-    let v_actual: Vec<f64> = state.iter().map(|s| s.y).collect();
-    let x_measured: Vec<f64> = measured.iter().map(|m| m.x).collect();
-    let v_measured: Vec<f64> = measured.iter().map(|m| m.y).collect();
-    let x_predicted: Vec<f64> = predicted.iter().map(|p| p.x).collect();
-    let v_predicted: Vec<f64> = predicted.iter().map(|p| p.y).collect();
-    let x_updated: Vec<f64> = updated.iter().map(|u| u.x).collect();
-    let v_updated: Vec<f64> = updated.iter().map(|u| u.y).collect();
+    // let t = (0..n_samples).map(|x| x as f64 * dt).collect::<Vec<f64>>();
+    // let x_actual: Vec<f64> = state.iter().map(|s| s.x).collect();
+    // let v_actual: Vec<f64> = state.iter().map(|s| s.y).collect();
+    // let x_measured: Vec<f64> = measured.iter().map(|m| m.x).collect();
+    // let v_measured: Vec<f64> = measured.iter().map(|m| m.y).collect();
+    // let x_predicted: Vec<f64> = predicted.iter().map(|p| p.x).collect();
+    // let v_predicted: Vec<f64> = predicted.iter().map(|p| p.y).collect();
+    // let x_updated: Vec<f64> = updated.iter().map(|u| u.x).collect();
+    // let v_updated: Vec<f64> = updated.iter().map(|u| u.y).collect();
 
-    // Plot positions
-    let root = BitMapBackend::new("kf_gps_pos.png", (1024, 768)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
-    let mut chart = ChartBuilder::on(&root)
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(
-            t.first().copied().unwrap()..t.last().copied().unwrap(),
-            x_actual.first().copied().unwrap()..x_actual.last().copied().unwrap(),
-        )
-        .unwrap();
-    chart.configure_mesh().draw().unwrap();
-    chart
-        .draw_series(LineSeries::new(t.iter().cloned().zip(x_actual), &RED))
-        .unwrap();
-    chart
-        .draw_series(LineSeries::new(t.iter().cloned().zip(x_measured), &BLUE))
-        .unwrap();
-    chart
-        .draw_series(LineSeries::new(t.iter().cloned().zip(x_predicted), &GREEN))
-        .unwrap();
-    chart
-        .draw_series(LineSeries::new(t.iter().cloned().zip(x_updated), &BLACK))
-        .unwrap();
-    root.present().unwrap();
+    // // Plot positions
+    // let root = BitMapBackend::new("kf_gps_pos.png", (1024, 768)).into_drawing_area();
+    // root.fill(&WHITE).unwrap();
+    // let mut chart = ChartBuilder::on(&root)
+    //     .margin(5)
+    //     .x_label_area_size(30)
+    //     .y_label_area_size(30)
+    //     .build_cartesian_2d(
+    //         t.first().copied().unwrap()..t.last().copied().unwrap(),
+    //         x_actual.first().copied().unwrap()..x_actual.last().copied().unwrap(),
+    //     )
+    //     .unwrap();
+    // chart.configure_mesh().draw().unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(t.iter().cloned().zip(x_actual), &RED))
+    //     .unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(t.iter().cloned().zip(x_measured), &BLUE))
+    //     .unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(t.iter().cloned().zip(x_predicted), &GREEN))
+    //     .unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(t.iter().cloned().zip(x_updated), &BLACK))
+    //     .unwrap();
+    // root.present().unwrap();
 
-    // Plot velocities
-    let root = BitMapBackend::new("kf_gps_vel.png", (1024, 768)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
-    let mut chart = ChartBuilder::on(&root)
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(
-            t.first().copied().unwrap()..t.last().copied().unwrap(),
-            v_actual.first().copied().unwrap()..v_actual.last().copied().unwrap(),
-        )
-        .unwrap();
-    chart.configure_mesh().draw().unwrap();
-    chart
-        .draw_series(LineSeries::new(t.iter().cloned().zip(v_actual), &RED))
-        .unwrap();
-    chart
-        .draw_series(LineSeries::new(t.iter().cloned().zip(v_measured), &BLUE))
-        .unwrap();
-    chart
-        .draw_series(LineSeries::new(t.iter().cloned().zip(v_predicted), &GREEN))
-        .unwrap();
-    chart
-        .draw_series(LineSeries::new(t.iter().cloned().zip(v_updated), &BLACK))
-        .unwrap();
-    root.present().unwrap();
+    // // Plot velocities
+    // let root = BitMapBackend::new("kf_gps_vel.png", (1024, 768)).into_drawing_area();
+    // root.fill(&WHITE).unwrap();
+    // let mut chart = ChartBuilder::on(&root)
+    //     .margin(5)
+    //     .x_label_area_size(30)
+    //     .y_label_area_size(30)
+    //     .build_cartesian_2d(
+    //         t.first().copied().unwrap()..t.last().copied().unwrap(),
+    //         v_actual.first().copied().unwrap()..v_actual.last().copied().unwrap(),
+    //     )
+    //     .unwrap();
+    // chart.configure_mesh().draw().unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(t.iter().cloned().zip(v_actual), &RED))
+    //     .unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(t.iter().cloned().zip(v_measured), &BLUE))
+    //     .unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(t.iter().cloned().zip(v_predicted), &GREEN))
+    //     .unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(t.iter().cloned().zip(v_updated), &BLACK))
+    //     .unwrap();
+    // root.present().unwrap();
 }
